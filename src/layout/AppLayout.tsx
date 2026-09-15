15885b010e4ea9c17c32302966895d1c9f29f76c
nukeop <12746779+nukeop@users.noreply.github.com>
2026-09-15 15:51:33 +0200

    Palette

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -7,7 +7,7 @@ type AppLayoutProps = {
 
 export function AppLayout({ children }: AppLayoutProps) {
   return (
-    <div className="flex h-dvh flex-col bg-neutral-950 text-neutral-100">
+    <div className="flex h-dvh flex-col bg-background text-background-fg">
       <TopBar />
       <main className="flex-1 overflow-auto">{children}</main>
     </div>
